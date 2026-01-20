ce9f21041c190dd86e9d6de4b5d83e2d10c595e0
HanudeepSetti <settihanudeep@gmail.com>
2026-01-20 22:00:27 +0530

    Add GridDistortion background to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,50 @@
+import GridDistortion from "./GridDistortion";
+
 function Hero() {
   return (
-    <section id="home" style={{ padding: "80px", textAlign: "center" }}>
-      <h1>Hi, I’m Hanudeep S 👋</h1>
-      <h3>Data Analyst | Aspiring Data Scientist</h3>
-      <p>Python • SQL • Power BI • Machine Learning</p>
+    <section
+      id="home"
+      style={{
+        position: "relative",
+        height: "600px",
+        overflow: "hidden",
+      }}
+    >
+      {/* BACKGROUND EFFECT */}
+      <div
+        style={{
+          position: "absolute",
+          inset: 0,
+          zIndex: 0,
+        }}
+      >
+        <GridDistortion
+          imageSrc="https://picsum.photos/1920/1080?grayscale"
+          grid={10}
+          mouse={0.1}
+          strength={0.15}
+          relaxation={0.9}
+        />
+      </div>
+
+      {/* FOREGROUND CONTENT */}
+      <div
+        style={{
+          position: "relative",
+          zIndex: 1,
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        <h1>Hi, I’m Hanudeep S 👋</h1>
+        <h3>Data Analyst | Aspiring Data Scientist</h3>
+        <p>Deep Learning • SQL • Power BI • Machine Learning</p>
+      </div>
     </section>
   );
 }
