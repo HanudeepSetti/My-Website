628d7a1a875f922d2c7daa75b10f00325f9ac084
HanudeepSetti <settihanudeep@gmail.com>
2026-01-23 23:24:38 +0530

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import About from "./components/About";
 
 
 
